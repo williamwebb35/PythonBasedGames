2f26c65cd0e6e9676ac5aed02f6145deaa48508c
William C. Webb <williamwebb35@gmail.com>
2018-11-15 10:21:51 -0800

    Add files via upload
    A simple Python script for a guessing game that prompts the user to guess a number between one and ten within three tries.

---
+++ GuessANumberOneToTen.py
@@ -0,0 +1,27 @@
+# One of the guided scripts generated during Codecademy's ....
+# ... Python 2 web tutorial
+# modifified slightly by me
+
+
+from random import randint
+
+
+random_number = randint(1, 10)
+
+guesses_left = 3
+
+print("To win, correctly guess a number between one and ten. You have three tries.")
+print("Are you a winner or a loser?")
+attempt = int(0)
+
+while guesses_left > 0:
+    odds = round((float((1/(10-attempt)))*100))
+    print("The odds of a corect guess are",odds,"%")
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == random_number:
+        print("You win, winner!")
+        break
+    guesses_left -= 1
+    attempt += 1
+else:
+    print("You lose, loser!")
