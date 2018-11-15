069a57a20aa53c1df6a86ebe263817c94b6756a9
William C. Webb <williamwebb35@gmail.com>
2018-11-15 10:37:21 -0800

    Update GuessANumberOneToTen.py

--- GuessANumberOneToTen.py
+++ GuessANumberOneToTen.py
@@ -10,18 +10,24 @@ random_number = randint(1, 10)
 
 guesses_left = 3
 
-print("To win, correctly guess a number between one and ten. You have three tries.")
 print("Are you a winner or a loser?")
+print("To win, correctly guess a number between one and ten.")
+print("You have three tries.")
+
 attempt = int(0)
 
 while guesses_left > 0:
     odds = round((float((1/(10-attempt)))*100))
     print("The odds of a corect guess are",odds,"%")
     guess = int(input("Guess a number between 1 and 10: "))
+    guesses_left -= 1
     if guess == random_number:
         print("You win, winner!")
         break
-    guesses_left -= 1
+    else:
+        if guesses_left > 0:
+            print("Try again.")
+    
     attempt += 1
 else:
     print("You lose, loser!")
